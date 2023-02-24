8d91e5bbd69fcee8518f2832bf5da413fc636e87
cwissy lol <104460485+cwissyy@users.noreply.github.com>
2023-02-23 22:02:51 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,11 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("                                       Da Hood Script", "DarkTheme")
+
+
+-- Main
+local Tab = Window:NewTab("Fly")
+local Section = Tab:NewSection("MAIN")
+
+Section:NewButton("Fly", "Makes You Fly", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/cwissyy/flybirdie/main/fly.lua"))()
+end)
